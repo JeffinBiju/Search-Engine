74856785711f047184bc72fde016aff01b74cdef
Jeffin Biju <ee19b085@smail.iitm.ac.in>
2022-01-10 00:56:41 +0530

    added crawler

--- backend/crawler/models.py
+++ backend/crawler/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Url(models.Model):
-    url = models.TextField()
+    url = models.TextField(unique = True)
 
     def _str_(self):
         return self.url
--- backend/crawler/views.py
+++ backend/crawler/views.py
@@ -5,6 +5,9 @@ from .models import Url, Site
 from rest_framework.response import Response
 from bs4 import BeautifulSoup
 import requests
+from rest_framework.decorators import action
+import urllib.request
+from urllib.parse import urljoin
 import re
 
 # Create your views here.
@@ -14,20 +17,37 @@ class UrlView(viewsets.ModelViewSet):
     queryset = Url.objects.all()
     serializer_class = UrlSerializer
 
+    @action(detail=False)
+    def crawl(self, request):
+        queryset = Url.objects.all()
+        for query in queryset.iterator():
+            url = query.url
+            try:
+                response = requests.get(url)
+            except:
+                continue
+            soup = BeautifulSoup(response.content, 'html.parser')
+            for link in soup.find_all('a', href=True):
+                if re.match('https.*', link.get('href')):
+                    urlobj = Url(url=link.get('href'))
+                    urlobj.save()
+                else:
+                    urlobj = Url(url=url+link.get('href'))
+                    urlobj.save()
+        return Response({'data':'data'})
+
 
 class SiteView(viewsets.ModelViewSet):
     serializer_class = SiteSerializer
     queryset = Site.objects.all() 
 
     def create(self, request):
-
         url = Url(url=request.data['site'])
         url.save()
-        req = requests.get(request.data['site'])
-        soup = BeautifulSoup(req.content, 'html.parser')
-        for link in soup.find_all('a', attrs={'href': re.compile("^http://")}):
-            print(link.get('href'))
-            url = Url(url=link.get('href'))
+        response = requests.get(request.data['site'])
+        soup = BeautifulSoup(response.content, 'html.parser')
+        for loc in soup.find_all('loc'):
+            url = Url(url=loc.string)
             url.save()
         return Response({'data':'data'})
     
